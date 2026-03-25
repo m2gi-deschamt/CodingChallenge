d44a5736dddf9fd47609f274bf158e137ecd0d44
Deschamps <omtd.deschamps@gmail.com>
2026-03-25 15:24:38 +0100

    read Jsoncpp documentation, add default values

---
+++ baseSwitcherAPI/controllers/readme.md
@@ -0,0 +1,8 @@
+# Learning
+"Members est un autre nom pour std::vector<String>" 
+```c++
+using Members = std::vector<String>; //new
+// or 
+typedef std::vector<String> Members; //old
+```
+
--- baseSwitcherAPI/controllers/v1_Base.cc
+++ baseSwitcherAPI/controllers/v1_Base.cc
@@ -6,12 +6,15 @@ using namespace v1;
 
 // curl -X POST "http://localhost:5555/v1/Base/?value=1011&base1=2&base2=10"
 
+
+
+
 void Base::convert(const HttpRequestPtr &req,
                    std::function<void (const HttpResponsePtr &)> &&callback) 
 {
-    auto json = req->getJsonObject();
+    std::shared_ptr<Json::Value> json = req->getJsonObject();
 
-    if (!json)
+    if (json.get() == NULL)
     {
         auto resp = HttpResponse::newHttpResponse();
         resp->setStatusCode(k400BadRequest);
@@ -20,18 +23,21 @@ void Base::convert(const HttpRequestPtr &req,
         return;
     }
 
-    std::string value = (*json)["value"].asString();
-    int base1 = (*json)["base1"].asInt();
-    int base2 = (*json)["base2"].asInt();
+    std::string value = (*json).get("value", "1011").asString();
+    int base1 = (*json).get("base1", 4).asInt();
+    int base2 = (*json).get("base2", 3).asInt();
 
     std::string val = from_X_to_10(value, base1);
     val = from_10_to_X(val, base2); 
 
     Json::Value res;
-    res["input"] = value;
-    res["base1"] = base1;
-    res["base2"] = base2;
-    res["result"] = val;
+    res["input"]["base2"] = base2;
+    res["input"]["base1"] = base1;
+    res["input"]["value"] = value;
+    res["output"]["base"] = base2;
+    res["output"]["value"] = val;
+    res["success"] = true;
+    res["status"] = "200";
 
     auto resp = HttpResponse::newHttpJsonResponse(res);
 
