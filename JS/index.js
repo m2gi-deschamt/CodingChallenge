d913e73353e3dd4033cce8807bb480820c89a30c
Deschamps <omtd.deschamps@gmail.com>
2026-04-26 17:27:37 +0200

    refactoring & add style

--- JS/index.js
+++ JS/index.js
@@ -1,25 +1,4 @@
-
-let count = 0;
-
-function updateCount() {
-    count++;
-    countDisplay.innerHTML = `Gg Thib, you got ${count} click`;
-}
-
 const myButton = document.getElementById("myFirstButton");
-
-myButton.addEventListener("click", () => {
-    //audioSheep.play();
-    updateCount();
-})
-
-const p = document.createElement("p");
-p.textContent = "Bonjour, je suis index.js";
-p.id = 'Hello';
-document.body.appendChild(p);
-
-Hello.style = "color: blue";
-
 
 class SoundButton extends HTMLElement {
     result = "<div>";
